d1461c5e5645404b0e4f4d4959d20d472ed4249d
zenith go <35967220+jooa1018@users.noreply.github.com>
2026-06-28 22:49:38 +0900

    Fix package imports for cloud deployment

--- dynamics_core/__init__.py
+++ dynamics_core/__init__.py
@@ -1,3 +1,3 @@
-from .parser import analyze_text, infer_requested_quantity
-from .strategy_engine import recommend_strategy
-from .feedback import build_diagnosis, expected_fbd_items, detect_misconceptions
+from dynamics_core.parser import analyze_text, infer_requested_quantity
+from dynamics_core.strategy_engine import recommend_strategy
+from dynamics_core.feedback import build_diagnosis, expected_fbd_items, detect_misconceptions
